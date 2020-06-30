cb9c7e6d05d41c7396c73d409ef1113066b162fd
chenrenhui <qixingzhi@syswin.com>
2020-06-30 19:45:21 +0800

    初始化

--- src/java/org/paumard/model/Actor.java
+++ src/java/org/paumard/model/Actor.java
@@ -18,7 +18,11 @@
 
 package org.paumard.model;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
 import java.util.Objects;
+import java.util.stream.Collectors;
 
 /**
  *
@@ -66,6 +70,77 @@ public class Actor {
     @Override
     public String toString() {
         return "Actor{" + "lastName=" + lastName + ", firstName=" + firstName + '}';
+    }
+
+    public static void main(String[] args) {
+
+        List<User> userList = new ArrayList<User>();
+        userList.add(new User("杨胜军", 23, '男'));
+        userList.add(new User("杨胜军", 23, '男'));
+        userList.add(new User("杨胜军", 22, '女'));
+        userList.add(new User("杨胜军", 23, '女'));
+        userList.add(new User("黄海明", 23, '男'));
+        userList.add(new User("黄海明", 22, '男'));
+        userList.add(new User("黄海明", 22, '女'));
+        userList.add(new User("黄海明", 23, '女'));
+        userList.add(new User("赵忠有", 23, '男'));
+        userList.add(new User("赵忠有", 22, '男'));
+        userList.add(new User("赵忠有", 22, '女'));
+        userList.add(new User("赵忠有", 23, '女'));
+
+        Map<String, List<User>> boxBarcodeMap2 = userList.stream().collect(Collectors.groupingBy(User::getName));
+        Map<String, Map<Integer, List<User>>> boxBarcodeMap3 = userList.stream().collect(
+                Collectors.groupingBy(User::getName, Collectors.groupingBy(User::getAge))
+        );
+        Map<String, Map<Integer, Long>> boxBarcodeMap = userList.stream().collect(
+                Collectors.groupingBy(User::getName, Collectors.groupingBy(User::getAge, Collectors.counting()))
+        );
+
+        for (Map.Entry<String, Map<Integer, Long>> childMap : boxBarcodeMap.entrySet()) {
+            System.out.print("姓名:" + childMap.getKey());
+            for (Map.Entry<Integer, Long> item : childMap.getValue().entrySet()) {
+                System.out.print(" \t 年龄:" + item.getKey() + " \t 数量:" + item.getValue());
+            }
+            System.out.print("\n");
+        }
+        for (Map.Entry<String, Map<Integer,  List<User>>> childMap : boxBarcodeMap3.entrySet()) {
+            System.out.print("姓名:" + childMap.getKey());
+            for (Map.Entry<Integer,  List<User>> item : childMap.getValue().entrySet()) {
+                System.out.print(" \t 年龄:" + item.getKey() + " \t 数量:" + item.getValue());
+            }
+            System.out.print("\n");
+        }
+    }
+    public static class User{
+
+
+        public User(String name, int age, char sex) {
+            super();
+            this.name = name;
+            this.age = age;
+            this.sex = sex;
+        }
+        private String name;
+        private int age;
+        private char sex;
+        public String getName() {
+            return name;
+        }
+        public void setName(String name) {
+            this.name = name;
+        }
+        public int getAge() {
+            return age;
+        }
+        public void setAge(int age) {
+            this.age = age;
+        }
+        public char getSex() {
+            return sex;
+        }
+        public void setSex(char sex) {
+            this.sex = sex;
+        }
     }
 }
 
